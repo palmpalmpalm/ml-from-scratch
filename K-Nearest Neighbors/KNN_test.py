2818f536ad57c4f9ee070d92ed86b270e4724049
Supawich Sitdhipol <Supawichs@gmail.com>
2021-03-02 16:59:06 +0700

    Move file to folder

--- K-Nearest Neighbors/KNN_test.py
+++ K-Nearest Neighbors/KNN_test.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])

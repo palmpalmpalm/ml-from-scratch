a6dc0377cf6ec111c833d44e507113f898e16b5b
Supawich Sitdhipol <Supawichs@gmail.com>
2021-03-09 21:24:28 +0700

    Naive Bayes Classifier (Gaussian Distribution)

---
+++ Naive Bayes/nb.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class NaiveBayes:
+
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.classes = np.unique(y)
+        n_classes = np.unique(y)
+
+        # init mean, var, priors
+        self._mean = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._var = np.zeros((n_classes, n_features), dtype=np.float64)
+        self._priors = np.zeros(n_classes, dtype=np.float64)
+
+        for c in self._classes:
+            pass
+
+    def predict(self,X):
+        pass
+
+if __name__ == '__main__':
+    test = NaiveBayes
